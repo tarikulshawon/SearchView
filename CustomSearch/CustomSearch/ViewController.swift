72ccc6f0844dd31959364a27942c8ccbe92e8b51
ti_shawon <33782384+tarikulshawon@users.noreply.github.com>
2024-07-11 00:06:45 +0600

    Add files via upload

---
+++ CustomSearch/CustomSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomSearch
+//
+//  Created by Tarikul Islam on 10/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
